86c9fc4758cc9d596caf94db5dff6d1331653514
Anwesa <anwesa2099@gmail.com>
2020-07-02 00:42:32 +0530

    Header and footer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+
 
 ReactDOM.render(
   <App />,

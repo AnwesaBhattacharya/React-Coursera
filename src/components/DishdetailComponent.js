834a7d72a54616d2621930ef9f24f630179ace7b
Anwesa <anwesa2099@gmail.com>
2020-07-04 18:13:26 +0530

    React Redux Forms Visited

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,9 +1,11 @@
 import React,{ Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Row, Col, Label, Button } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, LocalForm, Form, Errors, actions } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+
+
 
 
 const required = (val) => val && val.length;
